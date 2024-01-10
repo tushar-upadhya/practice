9d5983e3c13df3012c4a3920ab7cb35beb170060
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-01-10 10:40:39 +0530

    components

---
+++ reactJS/01/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Footer, Header } from "./components";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
